3662095f9dffad4d6d418413b5ff657caeb184c7
DrtSinX98 <pritish@stag-os.org>
2022-04-28 23:15:51 +0530

    spaced: lineage-ify
    Signed-off-by: DrtSinX98 <pritish@stag-os.org>

--- lineage_spaced.mk
+++ lineage_spaced.mk
@@ -21,13 +21,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/spaced/device.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_spaced
+PRODUCT_NAME := lineage_spaced
 PRODUCT_DEVICE := spaced
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme 8i
