0804368a3dbdf497e0b4dfde12365def6fa75955
INFIXHELL <mridulsharma57570@gmail.com>
2022-08-28 12:00:21 +0000

    spaced:updated some props

--- system_prop.mk
+++ system_prop.mk
@@ -61,3 +61,30 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=1440 \
     ro.zram.periodic_wb_delay_hours=24
+
+    # Dex2oat
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    dalvik.vm.dex2oat64.enabled=true \
+    dalvik.vm.image-dex2oat-filter=quicken \
+    dalvik.vm.image-dex2oat-threads=8 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.dex2oat-filter=quicken \
+    dalvik.vm.dex2oat-threads=8 \
+    dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7
+    # LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.psi_complete_stall_ms=150 \
+    ro.lmk.swap_free_low_percentage=20 \
+    ro.lmk.kill_timeout_ms=100
+
+    # Bionic
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.bionic.cpu_variant=cortex-a78 \
+    ro.bionic.2nd_cpu_variant=cortex-a78
+
+# Dalvik
+PRODUCT_PRODUCT_PROPERTIES += \
+    dalvik.vm.isa.arm.variant=cortex-a78 \
+    dalvik.vm.isa.arm64.variant=cortex-a78
